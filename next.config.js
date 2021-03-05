1bfb00ed10ce8ea355267feff5f91cd391c39910
Lucas Cardoso <lucasmatheussc97@gmail.com>
2021-03-05 06:54:00 -0300

    next.config  removido

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    env: {
-        DATABASE_PASSWORD=bTq5P3t1prHB7Nft,
-        DATABASE_NAME=ladi_presence_list,
-        APP_URL_DEV="localhost:3000",
-        APP_URL_PROD="https://"
-    }
-};
