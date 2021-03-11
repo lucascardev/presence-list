31de5ba03fc6be3f318faf69e90ec66fb6495ad8
Lucas Cardoso <lucasmatheussc97@gmail.com>
2021-03-11 18:19:39 -0300

    added string mongo

--- src/middlewares/mongodb.js
+++ src/middlewares/mongodb.js
@@ -2,13 +2,12 @@ import mongoose from 'mongoose';
 
 const connectDB = handler => async (req, res) => {
   try {
-    console.log('envs'+process.env.DATABASE_PASSWORD + process.env.DATABASE_NAME)
     if (mongoose.connections[0].readyState) {
         // Use current db connection
         return handler(req, res);
       }
       // Use new db connection
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(`${process.env.MONGODB_URI}`, {
         useUnifiedTopology: true,
         useFindAndModify: false,
         useCreateIndex: true,
